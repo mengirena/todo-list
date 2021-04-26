515ed19f3a5bf5e68f6d2d928481635dba7ccfa7
Irena Chen <irena1009@gmail.com>
2021-04-26 12:36:54 -0700

    add put method

--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
 const deleteButtons = document.querySelectorAll(".delete-btn") // nodeList
+const tasks = document.querySelectorAll(".task") // nodeList
+
 Array.from(deleteButtons).forEach(btn =>{
     btn.addEventListener("click", deleteTask)
 })
-console.log('btn')
+Array.from(tasks).forEach(task =>{
+    task.addEventListener("click", updateCompleteness)
+})
 
 async function deleteTask(){
-    console.log('indelete')
     const deleteId = this.parentNode.id //parentNode.id can get the id attribute data
     const response = await fetch("/deleteTask", {
         method:"delete",
@@ -15,5 +18,20 @@ async function deleteTask(){
         }) // parse JSON file to the server
     })
     console.log(response)
+    location.reload()
+}
+
+async function updateCompleteness(){
+    const taskId = this.id
+    const completeness = this.className
+    const result = await fetch('/updateCompleteness', {
+        method: 'put',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            'updateId': taskId,
+            'completeness': completeness.includes('completed')
+        })
+    })
+    console.log(result)
     location.reload()
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const MongoClient = require("mongodb").MongoClient //require mongodb to talk to the database
 const {v4:uuidv4} = require("uuid")
 
+
 const PORT = 2021
 
 app.set("view engine","ejs") // set the view engine using ejs
@@ -32,20 +33,30 @@ app.post('/createTask',async (request, response)=>{
 })
 //get task
 app.get('/', async (request, response)=>{
-    const data = await db.collection("Tasks").find().toArray()
+    const data = await db.collection("Tasks").find().sort({finished: 1, recordTime: 1}).toArray()
     response.render('index.ejs', {posts:data})
 })
 
-
 //delete task
-app.delete('/deleteTask', (request, response)=>{
-    console.log(request.body)
-    const result = db.collection('Tasks').deleteOne({id:request.body.deleteId})
+app.delete('/deleteTask', async (request, response)=>{
+    const result = await db.collection('Tasks').deleteOne({id:request.body.deleteId})
     console.log('data deleted')
     response.json('data deleted')
 })
-//update task
 
+//update task
+app.put('/updateCompleteness', async (request, response)=>{
+    const result = await db.collection('Tasks').updateOne({id:request.body.updateId},{
+       $set:{
+           finished: !request.body.completeness
+       }
+    },{
+        sort: {finished: -1, recordTime: 1},
+        upsert: false
+    })
+    console.log('completeness updated')
+    response.json("completeness updated")
+})
 
 //Listening to the port
 app.listen(process.env.PORT || PORT, ()=>console.log('server\'s running' ))
